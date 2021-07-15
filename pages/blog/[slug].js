95c4c7a2ef56d3940004820b0c21bc2bb565a94b
Aryamaan <62923164+arya1106@users.noreply.github.com>
2021-07-15 15:58:22 -0400

    testing netlify cms

---
+++ pages/blog/[slug].js
@@ -0,0 +1,43 @@
+import React from 'react';
+import fs from 'fs'
+import path from 'path';
+import matter from 'gray-matter';
+
+const Post  = ({contents, data}) => {
+  return <div>contents below
+    <h1>{data.title}</h1>
+    <h2>{data.date}</h2>
+    <h3>{data.description}</h3>
+    <pre>{contents}</pre>
+  </div>;
+};
+
+export const getStaticPaths = async () => {
+
+  const staticPaths = fs.readdirSync('./content');
+  const paths = staticPaths.map(filename => ({
+    params: {
+      slug: filename.replace('.md', '')
+    }
+  }))
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export const getStaticProps = async ({params: {slug}}) => {
+
+  const markdownWithMetaData = fs.readFileSync(path.join('./content', slug + '.md'), 'utf8');
+
+  const parsedMarkdown = matter(markdownWithMetaData);
+
+  return {
+    props: {
+      contents: parsedMarkdown.content,
+      data: parsedMarkdown.data
+    }
+  }
+}
+export default Post;

41ddd7f3ec92f6c210cffd41004243feeffdd445
Aryamaan <62923164+arya1106@users.noreply.github.com>
2021-07-17 20:33:04 -0400

    fix feed

--- pages/post.js
+++ pages/post.js
@@ -28,22 +28,10 @@ const Post = ({title, body, image, date}) => {
     return (
     <Card round='0' elevation='none' style={{'width':'auto'}} margin='small' background='light-1' flex='grow'>
         <CardHeader pad='medium' background='light-3'>{title}</CardHeader>
-          <Text size='large' color='dark-1' margin='small'>body</Text>
+          <Text size='large' color='dark-1' margin='small'>{body}</Text>
           <Box height='small' margin='small' width='small'><Image fit='contain' src={image} /> </Box>
-        <CardFooter pad='medium'>{"published on: "+date}</CardFooter>
+        <CardFooter pad='medium'>{"published on: "+{date}}</CardFooter>
     </Card> 
     )};
-
-
-export function getStaticProps() {
-    return {
-        props: {
-            title: 'Post',
-            body: 'This is a post',
-            image: 'https://placeimg.com/640/480/nature',
-            date: 'Today',
-        },
-    };
-}
 
 export default Post;

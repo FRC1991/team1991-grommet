ce873c712d44b62145f3e3679851b806862fffad
Aryamaan <62923164+arya1106@users.noreply.github.com>
2022-05-11 19:12:39 -0400

    fix config

--- next.config.js
+++ next.config.js
@@ -3,18 +3,18 @@ CopyPlugin = require('copy-webpack-plugin')
 module.exports = {
     "target" : "serverless",
     webpack5: true,
-    webpack: function(config, {dev, isServer}){
-        if (!isServer){
-            config.resolve.fallback.fs = false
-        }
-        if (!dev){
-            config.plugins.push(
-                new CopyPlugin({
-                    patterns: [{from : 'posts', to: 'posts'}],
-                })
-            )
-        }
-        return config},
+    // webpack: function(config, {dev, isServer}){
+    //     if (!isServer){
+    //         config.resolve.fallback.fs = false
+    //     }
+    //     if (!dev){
+    //         config.plugins.push(
+    //             new CopyPlugin({
+    //                 patterns: [{from : 'posts', to: 'posts'}],
+    //             })
+    //         )
+    //     }
+    //     return config},
     async redirects() {
       return [
         {

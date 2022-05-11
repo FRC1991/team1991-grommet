5641069433c57c5518b90c0f6678bbabc32a73a3
Aryamaan <62923164+arya1106@users.noreply.github.com>
2022-05-11 10:39:00 -0400

    get rid of post read workaround

--- pages/feed.js
+++ pages/feed.js
@@ -68,11 +68,7 @@ const feed = ({ posts }) => {
 };
 
 export async function getServerSideProps() {
-  let basePath = process.cwd();
-  if (process.env.NODE_ENV === "production") {
-    basePath = path.join(process.cwd(), ".next/server/chunks");
-  }
-  const files = fs.readdirSync(path.join(basePath, "posts"));
+  const files = path.resolve("./posts");
   var posts = [];
   files.forEach((file) => {
     const content = fs.readFileSync(

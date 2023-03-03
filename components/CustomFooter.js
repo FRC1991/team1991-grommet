aea30a3e118f1d3f91a97b2d1448350c54d8e485
Maanit Malhan <77414408+MaanitMalhan@users.noreply.github.com>
2023-03-02 21:07:44 -0500

    Update CustomFooter.js
    Added I maintained stuff I swear guys I want it for my resume ahahahahaha 💯

--- components/CustomFooter.js
+++ components/CustomFooter.js
@@ -4,7 +4,8 @@ const CustomFooter = (props) => (
   <Box align="center" pad="medium">
     <Text size="small">
       © UHSSE 4-H Robotics Team 1991{" "}{<Anchor href="/admin">The Dragons</Anchor>}, 2022. Developed by{" "}
-      {<Anchor href="https://github.com/arya1106/">Aryamaan Dhomne</Anchor>}.
+      {<Anchor href="https://github.com/arya1106/">Aryamaan Dhomne</Anchor>}. Maintained by {" "}
+       {<Anchor href="https://github.com/MaanitMalhan/">Maanit Malhan</Anchor>}
     </Text>
   </Box>
 );
